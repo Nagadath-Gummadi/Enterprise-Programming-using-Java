821c2574f05d4575ae496a84becd983d20f1dcf6
Nagadath Gummadi <51068943+Nagadath-Gummadi@users.noreply.github.com>
2020-04-16 23:03:48 +0530

    Update LabProg1.java

--- Program1/LabProg1.java
+++ Program1/LabProg1.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
+
 public class LabProg1 {
      public void createTable(String s) throws SQLException
     {
